05eb04de02d10f6bc6507a4d04fb3ed0b49b7eba
Unknown <kir_3@mail.ru>
2017-04-09 21:35:44 +0300

    SM3 MiniBatchKMeans done
    fit и predict можно ускорить примерно:
    
    def predict(self, data):
        a, b = data.shape
        t = np.sum((data.reshape(a, 1, b) - self.mass_centers)**2, axis=2).argmin(1)
        return self.mass_centers[t]

--- SM3/KMeans.py
+++ SM3/KMeans.py
@@ -1,81 +1,87 @@
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-from sklearn.cluster import KMeans
 
 params = {'figure.subplot.left': 0.0,
+          'figure.figsize': (10, 10),
           'figure.subplot.right': 1.0,
           'figure.subplot.bottom': 0.0,
           'figure.subplot.top': 1.0}
 
 plt.rcParams.update(params)
 fig = plt.figure()
-# ax = fig.gca(projection='3d')
 
 
-class KMean:  # или MiniBatchKMeans
-    def __init__(self, clusters):
-        self.clusters = clusters
+class KMean:
+    def __init__(self, clusters, timers = False):
+        self.__means_cfg = clusters
+        self.__timers = timers
 
     def __init_mass_centers(self):
-        self.d_depth = self.data.shape[1]
-        return np.random.randint(self.data.min(), self.data.max(), [self.clusters, self.d_depth])
+        self.d_depth = self.__data.shape[1]
+        return np.random.randint(self.__data.min(), self.__data.max(), [self.__means_cfg, self.d_depth])
 
     def __init_list_of_objects(self):
         list_of_objects = list()
-        for i in range(0, self.clusters):
+        for i in range(0, self.__means_cfg):
             list_of_objects.append(list())
         return list_of_objects
 
-    def __rclc_mass_centers(self, new_cnt):
+    def __rclc_means(self, new_cnt):
         for string_n in range(len(self.cent_data)):
             if len(self.cent_data[string_n]) > 0:
-                new_cen = np.zeros([1, self.data.shape[1]])
+                new_cen = np.zeros([1, self.d_depth])
                 for elem_s in range(len(self.cent_data[string_n])):
-                    new_cen = new_cen + self.data[self.cent_data[string_n][elem_s]]
+                    new_cen = new_cen + self.__data[self.cent_data[string_n][elem_s]]
                 dif_cen = (new_cen / (len(self.cent_data[string_n]))).astype(int)
-                if (self.cntrs[string_n] - dif_cen != np.zeros([1, self.data.shape[1]])).all():
+                if (self.__means[string_n] - dif_cen != np.zeros([1, self.d_depth])).all():
                     new_cnt += 1
-                    self.cntrs[string_n] = dif_cen
+                    self.__means[string_n] = dif_cen
         return new_cnt
 
     def fit(self, data_g):
         t1 = time.time()
-        self.data = data_g
-        self.cntrs = self.__init_mass_centers()
-        print("Dots moved:", "\t", "Step time sec:")
+        self.__data = data_g
+        self.__means = self.__init_mass_centers()
+        if self.__timers:
+            print("Dots moved:", "\t", "Step time sec:")
         new_cnt = 1
         while new_cnt > 0:
-            t0 = time.time()
+            if self.__timers:
+                t0 = time.time()
             new_cnt = 0
             self.cent_data = self.__init_list_of_objects()
-            for dot in range(len(self.data)):
-                clust_dist = np.sum(np.abs(self.cntrs - np.array([self.data[dot]] * 64)) ** 2, axis=-1) ** (1. / 2)
+            for dot in range(len(self.__data)):
+                sqr_diff = (self.__means - np.array([self.__data[dot]] * self.__means_cfg)) ** 2
+                clust_dist = np.sum(sqr_diff, axis=-1)
                 min_ind = clust_dist.argmin()
                 self.cent_data[min_ind].append(dot)
-            new_cnt = self.__rclc_mass_centers(new_cnt)
-            print("%11d \t %.3f" % (new_cnt, time.time() - t0))
-
-        print("Done in: ")
-        print("%.3fsec" % (time.time() - t1))
+            new_cnt = self.__rclc_means(new_cnt)
+            if self.__timers:
+                print("%11d \t %.3f" % (new_cnt, time.time() - t0))
+        if self.__timers:
+            print("Done in: ")
+            print("%.3fsec" % (time.time() - t1))
         return self
 
     def predict(self, data_g):
-        t1 = time.time()
+        if self.__timers:
+            t1 = time.time()
         predict_cent_data = self.__init_list_of_objects()
         for dot in range(len(data_g)):
-            clust_dist = np.sum(np.abs(self.cntrs - np.array([data_g[dot]] * 64)) ** 2, axis=-1) ** (1. / 2)
+            sqr_diff = (self.__means - np.array([data_g[dot]] * self.__means_cfg)) ** 2
+            clust_dist = np.sum(sqr_diff, axis=-1)
             min_ind = clust_dist.argmin()
             predict_cent_data[min_ind].append(dot)
 
         new_data = np.zeros(data_g.shape)
         for row in range(len(predict_cent_data)):
             for item in range(len(predict_cent_data[row])):
-                new_data[predict_cent_data[row][item]] = self.cntrs[row]
-        print("Done in: ")
-        print(time.time() - t1)
+                new_data[predict_cent_data[row][item]] = self.__means[row]
+        if self.__timers:
+            print("Done in: ")
+            print("%.3fsec" % (time.time() - t1))
         return new_data
 
 
@@ -89,7 +95,7 @@ data = image.reshape((image.shape[0] * image.shape[1], 3))
 print(image.shape)
 new_cntrs = KMean(clusters).fit(data)
 
-image_g = mpimg.imread('./mailru.jpg')
+image_g = mpimg.imread('./colors.jpg')
 data_g = image_g.reshape((image_g.shape[0]*image_g.shape[1], 3))
 new_image = new_cntrs.predict(data_g)
 print(image_g.shape)
--- SM3/MiniBatchKMeans.py
+++ SM3/MiniBatchKMeans.py
@@ -11,78 +11,100 @@ params = {'figure.subplot.left': 0.0,
 plt.rcParams.update(params)
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+# ax = fig.gca(projection='3d')
+
+
+class MiniBatchKMeans:
+    def __init__(self, clusters, batch_size=100, iterations=100):
+        self.__means_cfg = clusters
+        self.__batch_size = batch_size
+        self.__iterations = iterations
+
+    def __initialize_means(self):
+        means = np.zeros([self.__means_cfg, self.__features])
+        for i in range(self.__means_cfg):
+            index = np.random.randint(0, self.data.shape[0])
+            means[i] = self.data[index]
+        return means
+
+    def __select_batch(self):
+        batch = np.zeros([self.__batch_size, self.__features])
+        for j in range(self.__batch_size):
+            index = np.random.randint(0, self.data.shape[0])
+            batch[j] = data[index]
+        return batch
+
+    def __rclc_means(self):
+        for string_n in range(len(self.__cent_data)):
+            if len(self.__cent_data[string_n]) > 0:
+                new_cen = np.zeros([1, self.__features])
+                for elem_s in range(len(self.__cent_data[string_n])):
+                    new_cen = new_cen + self.__batch[self.__cent_data[string_n][elem_s]]
+                dif_cen = (new_cen / (len(self.__cent_data[string_n]))).astype(int)
+                if (self.__means[string_n] - dif_cen != np.zeros([1, self.__features])).all():
+                    self.__means[string_n] = dif_cen
+
+    def __init_list_of_objects(self):
+        list_of_objects = list()
+        for i in range(0, self.__means_cfg):
+            list_of_objects.append(list())
+        return list_of_objects
+
+    def __closest_means(self):
+        t = np.sum((self.data - self.__means) ** 2, axis=2).argmin(1)
+        return self.__means[t]
+
+    def fit(self, data_g):
+        self.__iterations = 10
+        self.data = data_g
+        self.__features = data.shape[1]
+
+        t0 = time.time()
+        self.__means = self.__initialize_means()
+
+        for i in range(self.__iterations):
+            self.__batch = self.__select_batch()
+            self.__cent_data = self.__init_list_of_objects()
+            for dot in range(self.__batch_size):
+                sqr_diff = (self.__means - np.array([self.__batch[dot]] * self.__means_cfg)) ** 2
+                clust_dist = np.sum(sqr_diff, axis=-1)
+                min_ind = clust_dist.argmin()
+                self.__cent_data[min_ind].append(dot)
+            self.__rclc_means()
+
+        print("Fit done in: ")
+        print("%.3fsec" % (time.time() - t0))
+        return self
+
+    def predict(self, data_g):
+        t0 = time.time()
+        predict_cent_data = self.__init_list_of_objects()
+        for dot in range(len(data_g)):
+            sqr_diff = (self.__means - np.array([data_g[dot]] * self.__means_cfg)) ** 2
+            clust_dist = np.sum(sqr_diff, axis=-1)
+            min_ind = clust_dist.argmin()
+            predict_cent_data[min_ind].append(dot)
+
+        new_data = np.zeros(data_g.shape)
+        for row in range(len(predict_cent_data)):
+            for item in range(len(predict_cent_data[row])):
+                new_data[predict_cent_data[row][item]] = self.__means[row]
+        print("Predict done in: ")
+        print("%.3fsec" % (time.time() - t0))
+        return new_data
+
 
 image = mpimg.imread('./mailru.jpg')
 data = image.reshape((image.shape[0] * image.shape[1], 3))
-clusters_num = 64
+clusters = 64
 
-DATA_SIZE = data.shape[0]
-FEATURES = data.shape[1]
-MEANS = clusters_num
-ITERATIONS = 10
-BATCH_SIZE = 10000
+new_means = MiniBatchKMeans(clusters).fit(data)
 
-data = np.ndarray([DATA_SIZE, FEATURES])
-clusters = np.ndarray([BATCH_SIZE,])
-batch = np.ndarray([BATCH_SIZE, FEATURES])
-means = np.ndarray([MEANS, FEATURES])
-centroids = np.ndarray([MEANS, FEATURES])
-cluster_counts = np.ndarray([MEANS, ])
-
-
-def initialize_means():
-    for i in range(MEANS):
-        index = np.random.randint() % DATA_SIZE
-        for j in range(FEATURES):
-            means[i][j] = data[index][j]
-
-
-def select_batch():
-    for i in range(BATCH_SIZE):
-        index = np.random.randint() % DATA_SIZE
-        for j in range(FEATURES):
-            batch[i][j] = data[index][j]
-
-
-def cache_clusters():
-    for i in range(BATCH_SIZE):
-        dist_min = float("inf")
-        for j in range(MEANS):
-            dist = 0
-            for k in range(FEATURES):
-                diff = batch[i][k] - means[j][k]
-                dist += diff * diff
-            if dist < dist_min:
-                dist_min = dist
-                clusters[i] = j
-
-
-def update_means():
-    for i in range(MEANS):
-        for j in range(FEATURES):
-            centroids[i][j] = means[i][j]
-    for i in range(BATCH_SIZE):
-        index = clusters[i]
-        cluster_counts[index] += 1
-        eta = 1 / cluster_counts[index]
-        for j in range(FEATURES):
-            means[index][j] = (1.0 - eta) * means[index][j] + eta * batch[i][j]
-    pass
-
-
-def calculate_error():
-    for i in range(MEANS):
-        dist = 0
-        for j in range(FEATURES):
-            diff = centroids[i][j] - means[i][j]
-            dist = diff * diff
-    pass
-
-for i in range(ITERATIONS):
-    select_batch()
-    cache_clusters()
-    update_means()
-    calculate_error()
-    pass
-
+image_g = mpimg.imread('./mailru.jpg')
+data_g = image_g.reshape((image_g.shape[0]*image_g.shape[1], 3))
+new_image = new_means.predict(data_g)
+print(image_g.shape)
+new_image = new_image.reshape((image_g.shape[0], image_g.shape[1], 3)).astype(np.uint8)
+plt.axis("off")
+plt.imshow(new_image)
+plt.show()
